4102b5774db38712fb7ae6cc4c306dff8286d7c4
Production044 <99043038+Production044@users.noreply.github.com>
2022-02-05 03:43:34 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,4 @@
+print("Hi all!") # print out
+print("It's my first lessons on Beetroot", end='') #parametr end on this line
+print("And now i write this code", end='\n\n\n') #parametr ends three times in a new line
+print("But my PyCharm sad: paramentr end and sep is invalid syntax", sep='_') #use parament sep for space
