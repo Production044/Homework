33d900955705925afa82f1446b9339dbb33675cf
Production044 <99043038+Production044@users.noreply.github.com>
2022-02-17 13:49:11 +0200

    Delete task2.py

--- task2.py
+++
@@ -1,4 +0,0 @@
-print("Hi all!") # print out
-print("It's my first lessons on Beetroot", end='') #parametr end on this line
-print("And now i write this code", end='\n\n\n') #parametr ends three times in a new line
-print("But my PyCharm sad: paramentr end and sep is invalid syntax", sep='_') #use parament sep for space
